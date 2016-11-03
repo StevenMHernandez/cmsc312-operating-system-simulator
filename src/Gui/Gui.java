e691cfcfbddde8c3b0ed4d7b6ae265685b763126
lobbypolice <nyaaLobby@Protonmail.Ch>
2016-11-03 11:36:05 -0400

    textfield

--- src/Gui/Gui.java
+++ src/Gui/Gui.java
@@ -1,8 +1,14 @@
 package Gui;
 
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.control.TextField;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 import javafx.application.Application;
 
@@ -10,20 +16,39 @@ import javafx.application.Application;
 public class Gui extends Application {
 
     Stage window;
+    BorderPane layout;
+    HBox lowerBox;
+    TextField textInput;
     Button button;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+
         window = primaryStage;
         window.setTitle("Simulator");
+
         button = new Button();
         button.setText("test");
         button.setOnAction(e -> displayBox.display("displaybox", "hi does this work"));
 
-        StackPane layout = new StackPane();
-        layout.getChildren().add(button);
+        textInput = new TextField("temporarily out of order");
+        textInput.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                if (event.getCode() == KeyCode.ENTER) {
+                    System.out.println(textInput.getText());
+                }
+            }
+        });
 
-        Scene scene = new Scene(layout, 300, 300);
+        layout = new Borderpane();
+        lowerBox = new HBox();
+        lowerbox.setPadding(new Insets(10, 10, 10, 10));
+        lowerbox.setSpacing(10);
+        lowerbox.getChildren().addAll(textInput, button);
+        layout.setBottom(lowerBox);
+
+        Scene scene = new Scene(layout, 300, 200);
         window.setScene(scene);
         window.show();
 
