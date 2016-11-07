3b8de19763d6ca74cc02eed0990d8d2b183fdd93
drbly <drbly7@gmail.com>
2016-11-07 13:41:14 -0500

    added queue to EventQueue

--- src/Components/EventQueue.java
+++ src/Components/EventQueue.java
@@ -1,13 +1,17 @@
 package Components;
+import java.util.ArrayList;
 
 public class EventQueue {
-
+	
+	private ArrayList<Event> queue = new ArrayList<>();
+	
     public void enQueue(Event event) {
-
+    	queue.add(event);
     }
 
     Event deQueue() {
-        return null;
+    	//remove first object
+        return queue.remove(0);
     }
 
 }
